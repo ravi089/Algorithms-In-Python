85d20289eff2c1211dc7644dec83a0597a06986e
Ravindra Parmar <ravindra089@gmail.com>
2018-05-30 17:27:28 +0530

    Delete print_single.py

--- Array/print_single.py
+++
@@ -1,8 +0,0 @@
-# Find an element which appears once where every element appears twice.
-
-def print_single(arr):
-    
-
-if __name__ == '__main__':
-    arr = [7, 3, 5, 4, 5, 3, 4]
-    print (print_single(arr))
